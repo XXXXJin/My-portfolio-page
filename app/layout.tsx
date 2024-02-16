8dfa1f9ece4ca1db52a3a2b9270a3c40c6875b3c
XXXXJin <jinhaixu@hotmail.co.jp>
2024-02-16 17:29:49 +0900

    ヘッダーコンポーネントの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,32 @@
-import type { Metadata } from 'next'
-import { Sawarabi_Gothic } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Sawarabi_Gothic } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
 
-const sawarabiGothic = Sawarabi_Gothic({weight: '400', subsets: ['cyrillic']})
+const sawarabiGothic = Sawarabi_Gothic({
+  weight: "400",
+  subsets: ["cyrillic"],
+});
 
 export const metadata: Metadata = {
-  title: 'My portfolio',
-  applicationName: 'My portfolio',
-  keywords: ['next.js', 'react', 'tailwind', 'typescript'],
-  creator: 'Haixu Jin',
-  description: 'Working as frontend developer for almost 4 years.',
-}
+  title: "My portfolio",
+  applicationName: "My portfolio",
+  keywords: ["next.js", "react", "tailwind", "typescript"],
+  creator: "Haixu Jin",
+  description: "Working as frontend developer for almost 4 years.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${sawarabiGothic.className} bg-gray-50 text-gray-950`}>{children}</body>
+      <body className={`${sawarabiGothic.className} bg-gray-50 text-gray-950`}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
