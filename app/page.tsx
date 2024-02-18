653fbd7be21629be8a4b8baea7324a50ff121ed4
XXXXJin <jinhaixu@hotmail.co.jp>
2024-02-18 19:20:59 +0900

    コンポーネントの配置とデータの追加、バグ修正、ダミー画像の追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import About from "@/components/about";
 import Intro from "@/components/intro";
+import ProjectList from "@/components/projectList";
+import SectionDivider from "@/components/sectionDivider";
 
 export default function LandingPage() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro></Intro>
+      <SectionDivider />
+      <About />
+      <ProjectList />
     </main>
   );
 }
